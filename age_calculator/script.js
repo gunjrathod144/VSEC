6600e69966a97fc762e394f2e60fff53da21157b
gunjrathod144 <gunjrathod27@gmail.com>
2026-08-10 11:07:56 +0530

    Update script.js

--- age_calculator/script.js
+++ age_calculator/script.js
@@ -1,28 +1,12 @@
 function calculateAge() {
-    let dob = document.getElementById("dob").value;
 
-    if (!dob) {
-        document.getElementById("result").innerHTML =
-            "Please select your date of birth";
-        return;
-    }
-
-    let birthDate = new Date(dob);
+    let dob = new Date(document.getElementById("dob").value);
     let today = new Date();
 
-    let ageYears = today.getFullYear() - birthDate.getFullYear();
-    let ageMonths = today.getMonth() - birthDate.getMonth();
-
-    if (today.getDate() < birthDate.getDate()) {
-        ageMonths--;
-    }
-
-    // Adjust years and months
-    if (ageMonths < 0) {
-        ageYears--;
-        ageMonths += 12;
-    }
+    let months =
+        (today.getFullYear() - dob.getFullYear()) * 12
+        + (today.getMonth() - dob.getMonth());
 
     document.getElementById("result").innerHTML =
-        "Your Age is " + ageYears + " years and " + ageMonths + " months";
+        "You are " + months + " months old";
 }
